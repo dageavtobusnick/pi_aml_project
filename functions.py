1033a8ab7de5b5c82e0be57ca5910393795ea388
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-25 22:47:26 +0500

    extract_predict

--- functions.py
+++ functions.py
@@ -1,7 +1,17 @@
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
 
 
 def tokenize_function(text):
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     tokenizer.save_pretrained("./finetuned-bert-news-classifier")
     return tokenizer(text, padding="max_length", truncation=True)
+
+def predict(text):
+    model = BertForSequenceClassification.from_pretrained(
+                        "./finetuned-bert-news-classifier")
+    inputs = tokenize_function(text)
+    outputs = model(**inputs)
+    predictions = torch.argmax(outputs.logits, dim=-1)
+    categories = ['World', 'Sports', 'Business', 'Sci/Tech']
+    return categories[predictions.item()]
