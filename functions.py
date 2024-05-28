054616982dc35ebfa3c7f2cbeff8d9c40f4a719f
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-29 04:25:44 +0500

    refactor

--- functions.py
+++ functions.py
@@ -1,19 +1,14 @@
 from transformers import AutoTokenizer, pipeline
 from transformers import AutoModelForSequenceClassification
 
-
-def create_classifier():
-    model_name = "typeform/distilbert-base-uncased-mnli"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    classifier = pipeline('zero-shot-classification',
-                          model=model,
-                          tokenizer=tokenizer)
-    return classifier
+model_name = "typeform/distilbert-base-uncased-mnli"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+classifier = pipeline('zero-shot-classification', model=model, tokenizer=tokenizer)
 
 
-def predict(text, labels):
-    classifier = create_classifier()
-    candidate_labels = labels
-    result = classifier(text, candidate_labels)
-    return result['labels'][0]
+def predict(texts, labels):
+    if not isinstance(texts, list):
+        texts = [texts] 
+    results = classifier(texts, labels)
+    return [(result['labels'][0], result['scores'][0]) for result in results]
