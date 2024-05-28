030dda9e117e29966303c5c10f6786dd39890d04
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-29 04:26:59 +0500

    fix

--- functions.py
+++ functions.py
@@ -4,11 +4,13 @@ from transformers import AutoModelForSequenceClassification
 model_name = "typeform/distilbert-base-uncased-mnli"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
-classifier = pipeline('zero-shot-classification', model=model, tokenizer=tokenizer)
+classifier = pipeline('zero-shot-classification',
+                      model=model,
+                      tokenizer=tokenizer)
 
 
 def predict(texts, labels):
     if not isinstance(texts, list):
-        texts = [texts] 
+        texts = [texts]
     results = classifier(texts, labels)
     return [(result['labels'][0], result['scores'][0]) for result in results]
