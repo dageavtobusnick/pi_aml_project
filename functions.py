003b56106cdc35e4c1ed551f6e5191a8308956e6
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-29 14:52:10 +0500

    add check

--- functions.py
+++ functions.py
@@ -10,7 +10,18 @@ classifier = pipeline('zero-shot-classification',
 
 
 def predict(texts, labels):
-    if not isinstance(texts, list):
+    if not isinstance(texts, (list, str)):
+        raise ValueError("The 'texts' parameter should be a string or a list of strings.")
+    if isinstance(texts, str):
         texts = [texts]
+
+    if not isinstance(labels, list) or not all(isinstance(label, str) for label in labels):
+        raise ValueError("The 'labels' parameter should be a list of strings.")
+    
+    if not texts:
+        raise ValueError("The 'texts' list should not be empty.")
+    if not labels:
+        raise ValueError("The 'labels' list should not be empty.")
+
     results = classifier(texts, labels)
     return [(result['labels'][0], result['scores'][0]) for result in results]
